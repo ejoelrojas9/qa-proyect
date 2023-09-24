2c395b434697949041f030d24a48301858b62776
ejoelrojas9 <96266652+ejoelrojas9@users.noreply.github.com>
2023-09-24 18:32:03 -0500

    Family test

--- test/integration/devise/login_success_spec.rb
+++ test/integration/devise/login_success_spec.rb
@@ -1,27 +1,17 @@
 require 'webdrivers/chromedriver'
-require 'byebug'
 require_relative ('../login_info.rb')
 
 describe "Given good credentials" do
-  it "should login" do
-    puts "\n\e[1;34m\e[47m -Running success login \e[m\n\n"
-    # Open @browser
-    @browser = Selenium::WebDriver.for :chrome
-    @browser.manage.window.maximize
-    # Navigate to url
-    @browser.navigate.to ($url)
-    # Enter email
-    @browser.find_element(id: 'admin_email').send_keys($email)
-    # Enter password
-    @browser.find_element(id: 'admin_password').send_keys($password)
-    # Click on login button
-    @browser.find_element(name: 'commit').click
-    # Check if success message is displayed
-    $wait.until{@browser.find_element(css: '.alert-alert').displayed?}
-    expect(@browser.find_element(css: '.alert-alert').text).to include($success_login_message)
-    # Close @browser
-    sleep(2)
-    @browser.quit
-    puts "Finished test"
+  it "Should login" do
+    explorer = Selenium::WebDriver.for :chrome
+    explorer.navigate.to ($url)
+    explorer.find_element(id: 'admin_email').send_keys("test@example.com")
+    explorer.find_element(id: 'admin_password').send_keys("test0102.")
+    explorer.find_element(name: 'commit').click
+    $wait.until{explorer.find_element(css: '.alert-alert').displayed?}
+    expect(explorer.find_element(css: '.alert-alert').text).to include("Signed in successfully.")
+    explorer.quit
+    puts "Test finished"  
   end
+  
 end
